77b831b8df36949537537e131b33617c335dbc7b
mooon-dev <zainab.42847@gmail.com>
2025-02-20 12:11:26 +0600

    ✈🚀

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 
+
 // src/App.jsx
 import React from 'react';
 import Navbar from './components/Navbar';
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-gray-900 text-white p-4 flex justify-between items-center">
+    <nav className="w-full bg-gray-1000 text-white p-4 flex justify-between items-center">
       <div className="text-2xl font-bold">WatchStore</div>
       <ul className="hidden md:flex space-x-4">
         <li>
